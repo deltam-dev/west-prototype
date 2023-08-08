8bc36b4bb4ca6ccfc272bead9449e6c53fd0197e
BlueFeet <b1feet.dev@uce.edu.ec>
2023-08-07 23:05:41 -0500

    kamikaze walk animation

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,16 +5,69 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
 
-    float speed = 1f;
+    private Transform target;
+    private Animator animator;
+    public float speed;
+    public bool follow;
+    public float animDistance;
+
+    public float atack;
+
     //bool 
     void Start()
     {
-        //GameState.health = hea;
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (follow)
+        {
+            animator.SetBool("isWalking", true);
+            SelectAnimation();
+            transform.position = Vector2.MoveTowards(transform.position,target.position,speed * Time.deltaTime);
+            Debug.Log("position:" + target.position);
+        }
+    }
+
+    private void SelectAnimation()
+    {
+        var distanciaX = Mathf.Abs(target.position.x - transform.position.x);
+
+        if (target.position.y < transform.position.y && distanciaX < animDistance)
+        {
+            // El objetivo está debajo
+            animator.SetFloat("walk", 0f);
+        }
+        else if (target.position.y > transform.position.y && distanciaX < animDistance)
+        {
+            // El objetivo está encima
+            animator.SetFloat("walk", 1f);
+        }
+        else
+        {
+            // El objetivo está en la misma altura que el personaje
+            animator.SetFloat("walk", 0.5f);
+
+            if (target.position.x < transform.position.x)
+            {
+                // El objetivo está a la izquierda
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else
+            {
+                // El objetivo está a la derecha
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+        }
+
+    }
+
+    private void StartFollow()
+    {
+        follow = true;
     }
 }
